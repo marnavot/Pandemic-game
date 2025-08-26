150050131147f95b9b6788787e7ed03034ce7240
marnavot <158297679+marnavot@users.noreply.github.com>
2025-08-26 17:46:25 +0300

    Update App.tsx
    fix error

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ export const App: React.FC = () => {
         handleResolveMorsTuaVitaMea, handleResolveHomoFaberFortunaeSuae, handleResolveAleaIactaEst, handleResolveAbundansCautelaNonNocet, handleResolveMeliusCavereQuamPavere,
         handleResolveMortuiNonMordent, handleResolveFestinaLente, handleResolveVeniVidiVici, handleResolveFreeBattle, handleResolveCarpeDiem, handlePurificationChoice,
         handleAgronomistPurifyChoice, handleNurseTokenPlacement, handleConfirmGovernmentMoves, handleHospitalFounding, handleResolveMailCorrespondence, handleResolveNewRails,
-        handleResolvePurifyWaterEvent, handleResolveRingRailroads,
+        handleResolvePurifyWaterEvent, handleResolveRingRailroads, handleResolveScienceTriumph,
     } = useGameLogic();
 
 
