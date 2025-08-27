7713b13cb4f2594acf78f57d6a57c7a4154eec70
marnavot <158297679+marnavot@users.noreply.github.com>
2025-08-27 18:50:38 +0300

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -600,9 +600,6 @@ const canRecruitArmy = inActionPhase &&
     PlayerRole.PraefectusFabrum,
   ]), []);
   
-
-  console.log('Dashboard: pawnToMove', pawnToMove);
-  console.log('Dashboard: currentPlayer', currentPlayer);
   return (
     <div className="h-full bg-gray-800 bg-opacity-80 backdrop-blur-sm p-2 flex flex-col text-sm space-y-2 rounded-lg shadow-lg overflow-y-auto">
       <div className="bg-gray-900 p-3 rounded-lg">
