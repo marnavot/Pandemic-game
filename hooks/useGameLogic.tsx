3f9399b32b29eeb028d61049e28a15bd79a7de84
marnavot <158297679+marnavot@users.noreply.github.com>
2025-09-04 20:51:20 +0300

    Update useGameLogic.tsx
    return to previous

--- hooks/useGameLogic.tsx
+++ hooks/useGameLogic.tsx
@@ -280,7 +280,10 @@ export const useGameLogic = () => {
 
     function _performInfection(gs: GameState, city: CityName, color: DiseaseColor, outbreaksInTurn: Set<CityName>, newlyOutbrokenCities: CityName[], cubesToAdd: number = 1, outbreakResults?: InfectionResult[]): InfectionResult {
         const result: InfectionResult = { city, color, defended: false, defenseType: null, legionsRemoved: 0, cubesAdded: 0, outbreak: false };
-
+        if (gs.gamePhase === GamePhase.GameOver) {
+            if (outbreakResults) outbreakResults.push(result);
+            return result;
+        }
         if (gs.gameType === 'iberia' && gs.nursePreventionTokenLocation) {
             const adjacentRegions = IBERIA_CITY_TO_REGIONS_MAP[city] || [];
             if (adjacentRegions.includes(gs.nursePreventionTokenLocation)) {
