4f9954c4829ba78fe4b1b535718814c933715fbd
marnavot <158297679+marnavot@users.noreply.github.com>
2025-09-06 11:58:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': `"${process.env.API_KEY}"`
-  },
-  preview: {
-    allowedHosts: ["pandemic-app-351119674329.europe-west1.run.app"]
   }
 })
